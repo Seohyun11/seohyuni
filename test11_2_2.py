d7799d3f0e259c27b308d15242590c000a0ccd36
seohyun11 <sagelsh@catholic.ac.kr>
2023-12-01 15:23:39 +0900

    23.11.27

---
+++ test11_2_2.py
@@ -0,0 +1,132 @@
+""" import pandas as pd
+
+target = "data/apt.csv"
+
+df = pd.read_csv(target, encoding="CP949")
+print(df.head())
+
+df.to_csv("data/apt.csv", encoding="utf8")
+
+df = pd.read_csv("data/apt.csv", index_col=0)
+
+
+df = df.rename(columns={"분양가격(제곱미터)":"분양가"})
+print(df)
+print(df.dtypes)
+
+df["분양가"] = df["분양가"].convert_dtypes()
+print(df.dtypes)
+print("\n--------------------------\n")
+
+
+res = df.sort_values(by="지역명")[:5]
+print(res)
+res = df.sort_values(by="지역명")
+res = df.sort_values(by="지역명")
+res = df.sort_values(by="지역명", ascending=False)
+res = df.sort_values(by="지역명", ascending=True) #디폴트
+print(res.head()) # n개 출력
+
+res = df.sort_values(by="연도")
+res = df.sort_values(by="분양가")
+res = df.sort_values(by="연도")[:5]
+print(res)
+print(res.head())
+
+
+res = df[["지역명"]][:5]
+res = df[["지역명"]]
+res = df[:, ["지역명", "연도","분양가"]] -> error
+res = df[:, ["지역명", "연도","분양가"]][:7]
+res = df[["지역명", "연도"]][:5]
+print(res)
+print("\n--------------------------\n")
+res = df.loc[:, ["지역명", "연도","분양가"]][:7]
+print(res)
+
+res = df.loc[:][:5]
+res = df.loc[:][:] -> 처음부터 끝까지 출력
+print(res)
+res = df.loc[:6, ["지역명", "연도"]] 6까지 출력
+res = df.loc[6:, ["지역명", "연도"]] 6부터 출력
+
+res = df.iloc[1] - > 행번호 1번 출력
+print(res)
+
+res = df.loc[:3, ["지역명", "연도"]][:8]
+print(res)
+
+
+res = df.loc[df["지역명"]=="서울",["지역명", "연도", "분양가"]]
+
+res = df.loc[df["지역명"]=="강원"]
+res = df.loc[df["연도"] > 2020]
+res = df.loc[df["지역명"]=="서울",["지역명", "연도", "분양가"]][:16]
+res = df.loc[df["지역명"]=="서울",["지역명", "연도", "분양가"]][-10:] -> 끝에서부터 출력
+print(res)
+
+df0 = df.copy()
+print(df0)
+print("\n--------------------------\n")
+res = df.iloc[2]
+res = df.iloc[2][2]
+print(res)
+
+
+res = df[df.index > 3560] # 3561번부터 출력
+print(res)
+
+
+res = df[df.연도 == 2023]
+res = df[df.월 == 3]
+res = df[(df.연도 == 2023) & (df.지역명 == "서울") & (df.월 == 6)]
+print(res)
+
+columns = list(df.columns)
+print(columns)
+
+df1 = df.reindex(index=df.index[:7], columns=list(df.columns) + ["extra"])
+df1 = df.reindex(columns=list(df.columns) + ["extra"])
+print(df)
+print("\n--------------------------\n")
+print(df1)
+
+print("\n--------------------------\n")
+df1.loc[:6, "extra"] = "0"
+df1.loc[:4, "extra"] = False
+print(df1)
+
+df2 = df1.copy()
+
+
+res = df2.dropna(how="any")
+res = df2.fillna(value="text")
+print(res)
+res = df2.fillna(value="1234")
+print(df2)
+print("\n--------------------------\n")
+
+res = df2.dropna(how="any", inplace=True) -> 삭제한다는 뜻
+print(res)
+print("\n--------------------------\n")
+print(df2)
+ """
+# Nan 데이터 출력
+res = pd.isna(df1)
+res = pd.isna(df)
+res = pd.isna(df0)
+res = pd.isna(df2)
+print(res)
+
+
+res = df["연도"].value_counts()
+res = df["지역명"].value_counts()
+res = df["월"].value_counts()
+res = df.월.value_counts()
+print(res)
+
+res = df.groupby(["지역명", "연도", "월"]).sum()
+res = df.groupby(["지역명", "연도", "월"])["분양가"].all("sum")
+res = df.groupby(["지역명", "연도", "월"])["분양가"].agg("sum")
+print(res)
+print(res)
